8eceb36186f328e19a15cdce901d12074cf085c1
cym2018. xyz <31214708+cym2018@users.noreply.github.com>
2019-05-22 20:19:29 +0800

    Update Action1.java

--- javaee/实验三/Action1.java
+++ javaee/实验三/Action1.java
@@ -3,7 +3,9 @@ package xyz.cym2018.struts;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
+import java.io.*;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 public class Action1 extends ActionSupport {
@@ -24,23 +26,53 @@ public class Action1 extends ActionSupport {
     public String getMajor() { return major; }
     public void setMajor(String major) { this.major = major; }
 
-    Date birthdate;
-    public Date getBirthdate() { return birthdate; }
-    public void setBirthdate(Date birthdate) { this.birthdate = birthdate; }
+    Date birthday;
+    public Date getBirthday() { return birthday; }
+    public void setBirthday(Date birthday) { this.birthday = birthday; }
 
     private String note;
     public String getNote() { return note; }
     public void setNote(String note) { this.note = note; }
 
+    private List<File> upload;
+    public List<File> getUpload() { return upload; }
+    public void setUpload(List<File> upload) { this.upload = upload; }
+
+    private List<String> uploadFileName;
+    public List<String> getUploadFileName() { return uploadFileName; }
+    public void setUploadFileName(List<String> uploadFileName) { this.uploadFileName = uploadFileName; }
+
+    private String password;
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+
     @Override
     public String execute() throws Exception {
+        setPassword(uploadFileName.get(0));
         Map request= (Map) ActionContext.getContext().get("request");
         request.put("username",username);
         request.put("name", name);
         request.put("major",major);
         request.put("note",note);
         request.put("gender",gender==1?"男":"女");
-        request.put("birthdate",birthdate);
+        request.put("birthday", birthday);
+        MysqlOperate.SaveStudentInfo(this);
+        SaveFile();
         return SUCCESS;
+    }
+
+    void SaveFile() throws Exception{
+        for(int i=0;i<upload.size();i++){
+            InputStream is = new FileInputStream(upload.get(i));
+            int index=uploadFileName.get(i).lastIndexOf(".");
+            OutputStream os = new FileOutputStream("C:\\Users\\cym20\\Saved Games\\Files\\save\\" + username+i+uploadFileName.get(i).substring(index));
+            byte buffer[] = new byte[1024];
+            int count = 0;
+            while ((count = is.read(buffer)) > 0) { os.write(buffer, 0, count); }
+            os.close();
+            is.close();
+        }
+
     }
 }
