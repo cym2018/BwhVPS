30c97ca97161bce1fedf6da73f0cc96508804e6c
cym2018. xyz <31214708+cym2018@users.noreply.github.com>
2019-05-22 14:31:58 +0800

    Create Interceptor1.java

---
+++ javaee/实验三_2/Interceptor1.java
@@ -0,0 +1,15 @@
+package xyz.cym2018.struts;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class Interceptor1 extends AbstractInterceptor {
+    @Override
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        Action1 action1= (Action1) actionInvocation.getAction();
+        // TODO:执行的操作.
+        if(action1.getName().equals("hello"))
+            return Action1.ERROR;
+        return actionInvocation.invoke();
+    }
+}
